95560372c5aa467dfeadc7d42eec78faa82f5a8a
Claydson <71528039+sicklex@users.noreply.github.com>
2022-05-26 13:54:08 -0300

    feat:Adicionando entidade mapping e migration de user

---
+++ Api/Ecommerce.EFCoreApi.Data/Mappings/UserMap.cs
@@ -0,0 +1,40 @@
+using Ecommerce.EFCoreApi.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ecommerce.EFCoreApi.Data.Mappings
+{
+    public class UserMap : IEntityTypeConfiguration<User>
+    {
+        public void Configure(Microsoft.EntityFrameworkCore.Metadata.Builders.EntityTypeBuilder<User> builder)
+        {
+            builder.ToTable("Users");
+
+            builder.HasKey(p => p.Id);
+
+            builder.Property(p => p.UserName)
+                .HasColumnType("varchar(255)")
+                .IsRequired();
+
+            builder.Property(p => p.Password)
+                .HasColumnType("varchar(255)")
+                .IsRequired();
+
+            builder.Property(p => p.FirstName)
+                .HasColumnType("varchar(255)")
+                .IsRequired();
+
+            builder.Property(p => p.LastName)
+                .HasColumnType("varchar(255)")
+                .IsRequired();
+
+            builder.Property(p => p.Telephone)
+                .HasColumnType("int")
+                .IsRequired();
+
+            builder.Property(p => p.Created_at)
+                .HasColumnType("datetime")
+                .IsRequired();
+
+        }
+    }
+}
